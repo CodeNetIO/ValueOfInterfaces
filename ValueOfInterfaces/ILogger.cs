dfeb435a4aeb8432678808586fb3bb06b0be7531
Darin Hoover <darinh@gmail.com>
2015-04-27 00:15:09 -0700

    Created ILogger.cs

---
+++ ValueOfInterfaces/ILogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueOfInterfaces
+{
+    /// <summary>
+    /// Represents a logger
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Writes text to the log
+        /// </summary>
+        /// <param name="text">The text to write to the log</param>
+        void WriteLine(string text);
+    }
+}
